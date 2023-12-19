27891dd847131cb4cc3154e350b4250b15f4d7a2
Pippa <carmela.pippa@hyperisland.se>
2023-12-19 22:13:23 +0100

    Add files in repository

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+  return (
+    <header>
+      <h1>My website</h1>
+      <h3>I am Carmela and this is my new website</h3>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </header>
+  );
+}
+
+export default Header;
