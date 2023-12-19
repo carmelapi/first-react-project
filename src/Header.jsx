6b5ad3cdd597ba35576aac384068f7dfd21485ce
Pippa <carmela.pippa@hyperisland.se>
2023-12-19 22:33:23 +0100

    Add key to the list

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,13 +1,17 @@
 function Header() {
+  const navBar = [
+    { id: 1, item: "Home" },
+    { id: 2, item: "About" },
+    { id: 3, item: "Contact" },
+  ];
+  const listNavBar = navBar.map((items) => (
+    <li key={items.id}>{items.item}</li>
+  ));
   return (
     <header>
       <h1>My website</h1>
       <h3>I am Carmela and this is my new website</h3>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-      </ul>
+      <ul>{listNavBar}</ul>
     </header>
   );
 }
